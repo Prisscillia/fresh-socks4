f290b8e7fba66307aef376d6ae1ea2d5688ba2e2
Prisscillia <mccoysebrell@gmail.com>
2021-02-17 18:34:34 -0500

    modified:   FreshSocks.sh 	modified:   checkedsocks 	new file:   cleansocks 	modified:   dirtysocks 	deleted:    checker.php?out=plain&action=export&working&groups=111260150

--- FreshSocks.sh
+++ FreshSocks.sh
@@ -11,20 +11,29 @@ touch dirtysocks
 		#Grab a fresh proxy list from github For Socks4 Proxies
 		curl 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt' -o laundrybag --silent ; 
 		echo "$(tail -n +2 laundrybag)" > laundrybag  #Cutting out Comments
-					# Grab 100 Hosts from the laundrybag and save them to dirtysocks
-					shuf -n 100 laundrybag | tee dirtysocks
-					# Format dirtysocks for the hidemy.name/api to check
-					sed -i -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' dirtysocks
+	# Grab 100 Hosts from the laundrybag and save them to dirtysocks
+	shuf -n 100 laundrybag | tee dirtysocks
+	# Format dirtysocks for the hidemy.name/api to check
+	sed -i -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' dirtysocks
+
 dirtysocks=$(cat dirtysocks)
 # Passing out data to hidemy.name/api
 curl -d "data=$dirtysocks" -X POST 'https://hidemy.name/api/checker.php?out=js&action=list_new&tasks=socks4&parser=lines' --silent | sed -e 's/^.*:/ /g' -e 's|[}]||g' | tee checkedsocks
 
 checkedsocks=$(cat checkedsocks | tr -d " " ) 	# Removing spaces so the API is happy
 
-sleep 1.5m
+sleep 1.8m
 
 # Grab file of responsive proxies from Hideme.name/api
-wget "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$checkedsocks"
+wget -O cleansocks "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$checkedsocks"
 
 		#Format proxies for ProxyChains.conf 
+		sed -i -e 's/^/SOCKS /' -e 's/:/ /' cleansocks
 
+######################################
+# Now comes the hard part of figuring out how to
+# Let proxychains come in contact with the data
+# Do I want to let this script run as root ? 
+# Do I want to let it have write permissions to my /etc/proxychains.conf
+# Can I only allow it permissions for that file ? 
+#  How do I get the OLD Proxies out of the .conf  ?
