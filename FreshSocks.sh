ec2ae8b0224608c843835f600f9df227a04f1c03
Prisscillia <mccoysebrell@gmail.com>
2021-02-17 18:00:57 -0500

    modified:   FreshSocks.sh 	new file:   checkedsocks 	new file:   checker.php?out=plain&action=export&working&groups=111260150 	new file:   dirtysocks 	new file:   laundrybag

--- FreshSocks.sh
+++ FreshSocks.sh
@@ -1,5 +1,9 @@
 !#/bin/bash
 
+# Creating our files so bash doesn't yell at us 
+touch checkedsocks
+touch dirtysocks
+
 #This is place holder text 
 # Will add something here that begins out tor service
 # and update our proxychains config file
@@ -7,18 +11,20 @@
 		#Grab a fresh proxy list from github For Socks4 Proxies
 		curl 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt' -o laundrybag --silent ; 
 		echo "$(tail -n +2 laundrybag)" > laundrybag  #Cutting out Comments
+					# Grab 100 Hosts from the laundrybag and save them to dirtysocks
+					shuf -n 100 laundrybag | tee dirtysocks
+					# Format dirtysocks for the hidemy.name/api to check
+					sed -i -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' dirtysocks
+dirtysocks=$(cat dirtysocks)
+# Passing out data to hidemy.name/api
+curl -d "data=$dirtysocks" -X POST 'https://hidemy.name/api/checker.php?out=js&action=list_new&tasks=socks4&parser=lines' --silent | sed -e 's/^.*:/ /g' -e 's|[}]||g' | tee checkedsocks
 
-		# Grab 100 Hosts from the laundrybag and save them to dirtySOCKS 
-		shuf -n 100 laundrybag | tee dirtySOCKS
+checkedsocks=$(cat checkedsocks | tr -d " " ) 	# Removing spaces so the API is happy
 
-		# Format dirtySOCKS for the hidemy.name/api to check
-		fmtSOCKS=$(sed -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' dirtySOCKS)
+sleep 1.5m
 
-		# Passing out data to hidemy.name/api and taking the group id to pull the response txt with 
-		# an active clients
-		checkedSOCKSGroup=$(curl -d "data=$fmtSOCKS" -X POST 'https://hidemy.name/api/checker.php?out=js&action=list_new&tasks=socks4&parser=lines' --silent | sed -e 's/^.*:/ /g' -e 's|[}]||g')
-		
-		# Grab file of responsive proxies from Hideme.name/api
-		curl "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$checkedSOCKSGroup" -o cleanlaudry --silent
+# Grab file of responsive proxies from Hideme.name/api
+wget "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$checkedsocks"
 
 		#Format proxies for ProxyChains.conf 
+
