99ddc1b2207cbac4eacd69c114b26cdce7878cd2
Prisscillia <78984291+Prisscillia@users.noreply.github.com>
2021-02-17 22:10:06 -0500

    Update freshsocks.sh

--- freshsocks.sh
+++ freshsocks.sh
@@ -31,25 +31,22 @@ fi
 if [[ ! -e $landrybag ]]; then
 	touch $laundrybag
 fi
-#This is place holder text 
-# Will add something here that begins out tor service
-# and update our proxychains config file
-# with fresh proxies
-#Grab a fresh proxy list from github For Socks4 Proxies
-curl 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt' -o $laundrybag --silent ; 
-sed -i -e 1,2d $laundrybag #Cutting out Comment
 
-# Grab 100 Hosts from the laundrybag and save them to dirtysocks
-shuf -n 100 $laundrybag | sed -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' > $dirtysocks #Format dirtysocks for the hidemy.name/api to check
+#	Grab a fresh Socks4 proxy list from TheSpeedX's Github
+		curl 'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt' -o $laundrybag --silent ; 
+		sed -i -e 1,2d $laundrybag #Cutting out Comment
 
-# Passing out data to hidemy.name/api
-curl -d "data=$(cat $dirtysocks)" -X POST 'https://hidemy.name/api/checker.php?out=js&action=list_new&tasks=socks4&parser=lines' --silent | sed -e 's/^.*:/ /g' -e 's|[}]||g' | tr -d " " | tee $checkedsocks
+#	Grab 100 Hosts from the laundrybag and save them to dirtysocks
+		shuf -n 100 $laundrybag | sed -e ':a' -e 'N' -e '$!ba' -e 's/:/%3A/g' -e 's/\n/%0A/g' > $dirtysocks #Format dirtysocks for the hidemy.name/api to check
+
+#	Passing out data to hidemy.name/api
+		curl -d "data=$(cat $dirtysocks)" -X POST 'https://hidemy.name/api/checker.php?out=js&action=list_new&tasks=socks4&parser=lines' --silent | sed -e 's/^.*:/ /g' -e 's|[}]||g' | tr -d " " | tee $checkedsocks
 
 sleep 1.8m
-# Grab file of responsive proxies from Hideme.name/api
-wget -O $cleansocks "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$(cat $checkedsocks)"
-#Format proxies for ProxyChains.conf 
-sed -i -e 's/^/socks4 /' -e 's/:/ /' $cleansocks
+#	Grab file of responsive proxies from Hideme.name/api
+		wget -O $cleansocks "https://hidemy.name/api/checker.php?out=plain&action=export&working&groups=$(cat $checkedsocks)"
+#	Format proxies for ProxyChains.conf 
+		sed -i -e 's/^/socks4 /' -e 's/:/ /' $cleansocks
 
 # W4rn1ng this is a personal configuration for MY BOX 
 #sed -i 116q /etc/proxychains4.conf 
